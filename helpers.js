00d4006316d6de1080d7691b02fea4924e87a5f8
Sagar Gopale <sagar090989@gmail.com>
2022-11-07 00:26:40 +0530

    Added catch block for promise calls

--- helpers.js
+++ helpers.js
@@ -3,6 +3,11 @@ const { createReadStream } = require('fs');
 const { getCoinCurrentValue } = require('./service');
 const { DateTime } = require('luxon');
 
+/**
+ * 
+ * @param {string} token Token name for which portfolio value is requested
+ * @returns A Promise with portfolio value by token or for all tokens
+ */
 const getCoinPortfolioValues = async (token) => {
     return new Promise(function (resolve, reject) {
         const rlInterface = require('readline').createInterface({
@@ -36,47 +41,48 @@ const getCoinPortfolioValues = async (token) => {
     });
 }
 
+/**
+ * 
+ * @param {string} date Date string in YYYY-MM-DD format
+ * @param {string} token Token name for which portfolio value is requested
+ * @returns A Promise with portfolio value by date and token or only date
+ */
 const getPortfolioValueByDate = async (date, token) => {
     return new Promise(function (resolve, reject) {
         const rlInterface = require('readline').createInterface({
             input: createReadStream(process.env.PORTFOLIO_CSV_FILE)
         });
         let result = {};
-        try {
-            // date format has to be ISO
-            const dateRangeStart = DateTime.fromISO(date).startOf('day').toMillis();
-            const dateRangeEnd = DateTime.fromISO(date).endOf('day').toMillis();
+        
+        const dateRangeStart = DateTime.fromISO(date).startOf('day').toMillis();
+        const dateRangeEnd = DateTime.fromISO(date).endOf('day').toMillis();
+        rlInterface.on('line', async (line) => {
+            let lineData = line.split(process.env.DELIMITER);
             
-            rlInterface.on('line', async (line) => {
-                let lineData = line.split(process.env.DELIMITER);
-                
-                const coin = lineData[2];
-                if(coin !== 'token') {
-                    const timestamp = lineData[0];
-                    if (timestamp >= dateRangeStart && timestamp <= dateRangeEnd) {
-                        const tradeType = lineData[1];
-                        const amount = lineData[3];
-                        let currentCountResult = result[coin] || { amount: 0 };
+            const coin = lineData[2];
+            if(coin !== 'token') {
+                const timestamp = lineData[0];
+                if (timestamp >= dateRangeStart && timestamp <= dateRangeEnd) {
+                    const tradeType = lineData[1];
+                    const amount = lineData[3];
+                    let currentCountResult = result[coin] || { amount: 0 };
 
-                        currentCountResult.amount -= tradeType === 'WITHDRAWAL' && parseFloat(amount);
-                        currentCountResult.amount += tradeType === 'DEPOSIT' && parseFloat(amount);
-                        result[coin] = currentCountResult;
-                    }
+                    currentCountResult.amount -= tradeType === 'WITHDRAWAL' && parseFloat(amount);
+                    currentCountResult.amount += tradeType === 'DEPOSIT' && parseFloat(amount);
+                    result[coin] = currentCountResult;
                 }
-            })
+            }
+        })
 
-            rlInterface.on('close', async () => {
-                const priceResult = await getCoinCurrentValue(Object.keys(result));
-                for(key in result) {
-                    const currentCoin = result[key];
-                    currentCoin.portfolioValue = `${currentCoin.amount * priceResult[key].USD} USD`;
-                }
-                const f = token ? result[token] : result;
-                resolve(f);
-            });
-        } catch(error) {
-            console.log('Error occurred: ', error);
-        }
+        rlInterface.on('close', async () => {
+            const priceResult = await getCoinCurrentValue(Object.keys(result));
+            for(key in result) {
+                const currentCoin = result[key];
+                currentCoin.portfolioValue = `${currentCoin.amount * priceResult[key].USD} USD`;
+            }
+            const f = token ? result[token] : result;
+            resolve(f);
+        });
     });
 }
 
--- portfolio.js
+++ portfolio.js
@@ -4,18 +4,18 @@ const { argv } = require('yargs');
 
 if(!argv.token && !argv.date) {
     console.log(`Fetching portfolio for all the investments...`);
-    getCoinPortfolioValues().then(result => console.log(result));
+    getCoinPortfolioValues().then(result => console.log(result)).catch(error => { console.log(error.message) });
 } else if(argv.token && !argv.date) {
     isValidToken(argv.token);
     console.log(`Fetching portfolio for token ${argv.token}...`);
-    getCoinPortfolioValues(argv.token).then(result => console.log(result));
+    getCoinPortfolioValues(argv.token).then(result => console.log(result)).catch(error => { console.log(error.message) });
 } else if(!argv.token && argv.date) {
     isValidDate(argv.date);
     console.log(`Fetching portfolio for date ${argv.date}...`);
-    getPortfolioValueByDate(argv.date).then(result => console.log(result));
+    getPortfolioValueByDate(argv.date).then(result => console.log(result)).catch(error => { console.log(error.message) });
 } else if(argv.token && argv.date) {
     isValidDate(argv.date);
     isValidToken(argv.token);
     console.log(`Fetching portfolio for all token ${argv.token} for date ${argv.date}...`);
-    getPortfolioValueByDate(argv.date, argv.token).then(result => console.log(result));
+    getPortfolioValueByDate(argv.date, argv.token).then(result => console.log(result)).catch(error => { console.log(error.message) });
 }
